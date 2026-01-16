877ba9d73d0b54154e74b361fb3f8caa8efe5943
Jasper Wu <182767243@qq.com>
2026-01-16 18:07:43 +0800

    Initial Commit

---
+++ Statio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Statio
+//
+//  Created by Jasper Wu on 2026/1/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Statio/StatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatioApp.swift
+//  Statio
+//
+//  Created by Jasper Wu on 2026/1/16.
+//
+
+import SwiftUI
+
+@main
+struct StatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
